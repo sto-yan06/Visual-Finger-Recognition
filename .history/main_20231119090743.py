25a5a600e1aa993124a9b5262585c1ae366bd64e
S.g <stoian.george06@gmail.com>
2023-11-19 15:08:14 +0100

    Minor touches

--- .history/main_20231119090743.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import numpy as np
-from pynput.mouse import Controller
-
-mouse = Controller()
-
-def detectHand(frame):
--- .history/main_20231119090840.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-from pynput.mouse import Controller
-
-mouse = Controller()
-
-def detectHand(frame):
-    hsv = cv2.cvtColor(frame, cv2.)
